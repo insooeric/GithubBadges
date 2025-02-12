193e04839ae9e9a7b08a705b025ba7b53ce6f1e8
Geek <insooeric.son@gmail.com>
2025-02-11 20:24:31 -0500

    PUSH for deploy

--- GithubBadges/Controllers/BadgeController.cs
+++ GithubBadges/Controllers/BadgeController.cs
@@ -126,7 +126,7 @@ namespace GithubBadges.Controllers
 
                 string finalSVG = ImageHelper.ConvertToSVG(request.BadgeFile, fileName);
 
-                // Console.WriteLine(finalSVG);
+                //Console.WriteLine(finalSVG);
 
                 // return BadRequest(new { Message = $"Error: Testing" }); // MAKE SURE TO REMOVE THIS LINE
 
