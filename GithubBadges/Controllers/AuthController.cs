7e6dd80d7f76d7da84bd368508aeba2af2d47a0e
Geek <insooeric.son@gmail.com>
2025-02-17 17:11:18 -0500

    resolving cookie error in android/ios devices

--- GithubBadges/Controllers/AuthController.cs
+++ GithubBadges/Controllers/AuthController.cs
@@ -42,7 +42,7 @@ namespace GithubBadges.Controllers
                 HttpOnly = true,
                 Secure = true,
                 SameSite = SameSiteMode.None,
-                Domain = "badgehub.vercel.app",
+                Domain = ".badgehub.vercel.app",
                 Path = "/",
                 Expires = DateTime.UtcNow.AddHours(1)
             });
